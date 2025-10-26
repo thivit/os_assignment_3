a1155ae7aba0593c0fc91839bf5b88b599365502
thivit <114897003+thivit@users.noreply.github.com>
2025-10-26 23:17:57 +1030

    implement mergesort.

--- comp2002-os-mergesort/mergesort.c
+++ comp2002-os-mergesort/mergesort.c
@@ -12,7 +12,18 @@ void merge(int leftstart, int leftend, int rightstart, int rightend){
 }
 
 /* this function will be called by parallel_mergesort() as its base case. */
-void my_mergesort(int left, int right){
+void my_mergesort(int left, int right)
+{
+	// return if single element in array.
+	if (left >= right) return;
+
+	// divide into two subarrays
+	int mid = left + (left + right) / 2;
+	my_mergesort(left, mid);
+	my_mergesort(mid + 1, right);
+
+	// merge subarrays.
+	merge(left, mid, mid + 1, right);
 }
 
 /* this function will be called by the testing program. */
